d63c089f3550019e5d7b769f51cc4addba58ba49
kicap1992 <kicap.karan92@gmail.com>
2022-08-10 22:43:46 +0800

    first commit

--- routes/from_esp32_routes.js
+++ routes/from_esp32_routes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 require('dotenv/config');
 const { deviceModel, newDeviceModel, notificationModel } = require('../models/device_model');
 const io_sock = require("socket.io-client");
-const socket = io_sock(`${process.env.HOST_URL}/`);
+const socket = io_sock(`https://tanah-longosor-be.herokuapp.com`);
 
 
 
 
 async function update_device_data(device_id, lat, lng, status) {
-  // console.log(device_id)
+  console.log("disini update device data")
   function send_notif(message, id, status, lat, lng) {
     // console.log(lat, "ini lat");
     // console.log(lng , "ini lng");
